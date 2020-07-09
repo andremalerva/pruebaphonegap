f780e751a2a4c6da72dcdfca3e501dc16e77117f
Blanca Malerva <blanca.malerva@gmail.com>
2020-07-09 14:46:49 -0500

    plugins

---
+++ cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+              module.exports = [
+                      /*          {
+                                "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser-resp.js",
+                                "id": "org.apache.cordova.inappbrowser.inappbrowser",
+                                "clobbers": [
+                                             "window.open"
+                                             ]
+                                },
+                                {
+                                "file": "plugins/org.apache.cordova.file-transfer/www/FileTransfer.js",
+                                "id": "org.apache.cordova.file-transfer.file-transfer"
+                                },
+                                {
+                                "file": "plugins/org.apache.cordova.file/www/File.js",
+                                "id": "org.apache.cordova.file.file"
+                                } */
+                                ];
+              module.exports.metadata = 
+              // TOP OF METADATA
+              {
+           /*   "org.apache.cordova.inappbrowser": "0.5.2",
+              "org.apache.cordova.file-transfer": "1.6.3",
+              "org.apache.cordova.file": "4.3.3" */
+              }
+              // BOTTOM OF METADATA
+              });
